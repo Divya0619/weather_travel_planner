a2efb583c596a3835ad6756a3ebe7e2664f6e26a
Divya0619 <divyabaskar0619@gmail.com>
2025-04-18 00:07:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import requests
 import mysql.connector
+import os
 
 app = Flask(__name__) 
 
@@ -239,5 +240,5 @@ def travelling():
     
   
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
-    
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
